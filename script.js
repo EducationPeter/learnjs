47ddc5efa923b093da82d105103fe6cfa0025386
EducationPeter <100352352+EducationPeter@users.noreply.github.com>
2022-02-25 15:30:51 +0100

    add console

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function foo() {
-
     let a = 0;
     let out = "hello"
-    
+    let a = 
     document.getElementById("out").innerHTML = out;
+  }
+
+  function consolelog(str)
+  {
+      content = document.getElementById("console").innerHTML
+      document.getElementById("console").innerHTML + str  
   }
